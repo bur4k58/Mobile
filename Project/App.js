2000657459bde4c8ddb2dbb8ba74fecb584dbaa3
bur4k58 <s116768@ap.be>
2020-11-22 16:55:11 +0100

    z

--- Project/App.js
+++ Project/App.js
@@ -6,69 +6,36 @@ import {NavigationContainer} from "@react-navigation/native";
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-community/async-storage';
 import MapView, {Marker} from 'react-native-maps';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import * as Location from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Foundation } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-      screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
 
-            if (route.name === 'Map') {
-              iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
-            } else if (route.name === 'List') {
-              iconName = focused ? 'ios-list-box' : 'ios-list';
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}>
-
-        <Tab.Screen name ="Map" component={MapScreen} />
-        <Tab.Screen name ="List" component={ListScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
-
+//Functie Map
 export const MapScreen = (props) =>{
   const [locatie, setLocatie] = useState(undefined);
-  
-  async function GetMapData() {
-    try {
-      let response = await fetch(
-        'https://api.jsonbin.io/b/5fba4cffa825731fc0a115f5/1',
-      );
-      let responseJson = await response.json();
-      const jsonValue = JSON.stringify(responseJson)
-      await AsyncStorage.setItem('@storage_Key', jsonValue)
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
   return(
     <View style={styles.container}>
-      {locatie &&
-      <MapView style={{flex:1}}
+      <MapView style={{width:"100%", height:"100%"}}
         region={{
-          latitude: locatie.latitude,
-          longitude: locatie.longitude,
-          latitudeDelta: locatie.latitudeDelta,
-          longitudeDelta: locatie.longitudeDelta
-        }}
+        latitude: 50.814604,
+        longitude: 4.386932,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+       }}
       >
-      </MapView>}
-      </View>
+      <Marker
+        key="Marker1"
+        coordinate={{latitude: 50.814604, longitude: 4.386932}}
+        title="AP Hogeschool"
+        description="Hier zijn we"
+      />
+      </MapView>
+   </View>
   )
 }
 
+//Functie Lijst
 export const ListScreen = () =>{
   return(
     <View>
@@ -77,8 +44,24 @@ export const ListScreen = () =>{
   )
 }
 
-
+//App
 const Tab = createBottomTabNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name ="Map" component={MapScreen} options={{tabBarIcon: ({color, size}) => (
+          <MaterialIcons name="map" size={24} color="black" />
+        )}
+        }/>
+        <Tab.Screen name ="List" component={ListScreen} options={{tabBarIcon: ({color, size}) => (
+          <Foundation name="list" size={24} color="black" />
+        )}
+        }/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
