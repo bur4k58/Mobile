96a92572fbb98aba908f8c59220fb0b12b5b3b31
bur4k58 <s116768@ap.be>
2020-12-01 14:48:39 +0100

    Marker toevoegen locatie en project structureren

--- Project/App.js
+++ Project/App.js
@@ -15,18 +15,7 @@ import { Button } from 'react-native-paper';
 import { color, Value } from 'react-native-reanimated';
 import { ActivityIndicator, Colors, Appbar } from 'react-native-paper';
 
-//locatie ophalen van gebruiker
-export const LocationView = () => {
-  const [loc, setLoc] = useState("")
-  useEffect(() => {
-    (async () => {
-      let position = await Location.getCurrentPositionAsync();
-      setLoc(position.coords);
-    })();
-  }, []);
-  return loc;
-}
-
+//Functie Datainladen
 export const dataInladen = (props) => {
   useEffect(() => {
     fetch('https://api.jsonbin.io/b/5fc054fc177c556ef9b35636/1')
@@ -43,9 +32,168 @@ export const dataInladen = (props) => {
   }, []);
 }
 
+const Stack = createStackNavigator();
+export const MapStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="MapScreen" component={MapScreen} options={{
+        title: "Park & Ride Antwerpen", headerStyle: { backgroundColor: "red" }, headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }} />
 
+      <Stack.Screen name="DetailsMap" component={DetailMapButton} />
+    </Stack.Navigator>
+  )
+}
 
+export const ListStack = () => {
+  return (
 
+    <Stack.Navigator>
+      <Stack.Screen name="ListScreen" component={ListScreen} options={{
+        title: "Park & Ride Antwerpen", headerStyle: { backgroundColor: "red" }, headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }} />
+      <Stack.Screen name="DetailsMap" component={DetailMapButton} />
+    </Stack.Navigator>
+  )
+}
+
+//Functie Detail knop map
+export const DetailMapButton = ({ route }) => {
+  
+  return (
+    <View style={styles.detailPaginaVanMap}>
+      <View style={styles.detailPaginaVanMapView}><Text style={{ color: "white", fontSize: 20 }}>{route.params.data.Naam}</Text></View>
+      <TextInput style={styles.detailPaginaTitle} editable={false} value={'Naam'} />
+      <Text style={styles.detailPaginaText}>{route.params.data.Naam}</Text>
+      <TextInput style={styles.detailPaginaTitle2} editable={false} value={'Adres'} />
+      <Text style={styles.detailPaginaText} >{route.params.data.Gemeente + " " + route.params.data.Postcode}</Text>
+      <TextInput style={styles.detailPaginaTitle2} editable={false} value={'Aantal Plaatsen'} />
+      <Text style={styles.detailPaginaText} >{route.params.data.Plaatsen}</Text>
+      <View style={styles.detailPaginaVanMapView}><Text style={{ color: "white", fontSize: 20 }}>Bereikbaar Met</Text></View>
+      <TextInput style={styles.detailPaginaTitle2} editable={false} value={'Buslijnen'} />
+      <Text style={styles.detailPaginaText} >{route.params.data.Buslijnen}</Text>
+      <TextInput style={styles.detailPaginaTitle2} editable={false} value={'Tramlijnen'} />
+      <Text style={styles.detailPaginaText} >{route.params.data.Tramlijnen}</Text>
+    </View>
+  )
+}
+
+//Functie Map navigatie
+export const MapScreen = ({ navigation }) => {
+  const [markerClickt, setMarkerClickt] = useState(false)
+  const [momdata, setMomdata] = useState("")
+  const [data, setData] = useState([]);
+  const [locatie, setLocatie] = useState("loading")
+
+  //Data inladen
+  dataInladen(setData);
+
+  //Locatie ophalen
+  useEffect(() => {
+    (async () => {
+      let position = await Location.getCurrentPositionAsync();
+      setLocatie(position.coords);
+    })();
+  }, []);
+
+  //
+  const detailView = () => {
+    return (
+      setMarkerClickt(true)
+    )
+  }
+
+  const detailMarker = (val) => {
+    return (
+      <View style={styles.detailMarkerView}>
+        <Text style={{ color: "black", fontSize: 20, marginTop: 10 }}>{val.Naam}</Text>
+        <Text style={{ color: "black", fontSize: 18 }} >{val.Gemeente + " " + val.Postcode}</Text>
+        <Button style={{ backgroundColor: "#ff1a1a", width: "80%", marginTop: 20 }}
+          onPress={() => { navigation.navigate('DetailsMap', { data: val }) }}
+        ><Text style={{ color: "white" }}>Details</Text></Button>
+      </View>
+    )
+  }
+
+  //
+  if (locatie == "loading") {
+    return <ActivityIndicator style={styles.dataLoading} animating={true} size="large" color={Colors.red800} />
+  }
+  else 
+  {
+    if (data == false) 
+    {
+      return <ActivityIndicator style={styles.dataLoading} animating={true} size="large" color={Colors.red800} />
+    } 
+    else 
+    {
+      return (
+        <View>
+          <MapView style={{ width: "100%", height: "100%", zIndex: 0 }} region={{
+            latitude: locatie.latitude,
+            longitude: locatie.longitude,
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5
+          }}>
+            <Marker 
+              key="Locatie"
+              coordinate={{latitude: locatie.latitude, longitude: locatie.longitude}}
+              title="Locatie"
+              description="U locatie"    
+            />
+
+            {data.map(val => {
+              return (
+                <Marker
+                  key={val.Naam}
+                  coordinate={{ latitude: val.Latitude, longitude: val.Longitude }}
+                  image={require('../Project/assets/parking.png')}
+                  onPress={() => {
+                    detailView()
+                    setMomdata(val)
+                  }}>
+                </Marker>
+              )
+            })}
+          </MapView>
+          {(markerClickt ? detailMarker(momdata) : <Text>False</Text>)}
+        </View>)
+    }
+  }
+}
+
+//Functie List navigatie
+export const ListScreen = ({ navigation }) => {
+
+  const [data, setData] = useState([]);
+  dataInladen(setData)
+
+  return (
+    <View style={{ marginTop: 15, backgroundColor: "white" }}>
+      <ScrollView>
+        {data.map((val) => {
+          return (
+            <TouchableOpacity key={val.OBJECTID} onPress={() => { navigation.navigate('DetailsMap', { data: val }) }}>
+              <View style={{ width: "100%", height: 50, margin: 5, backgroundColor: "red", }}>
+                <Text style={{ color: "white", fontSize: 15, marginTop: 10, justifyContent: "center", alignSelf: "center" }}>{val.Naam}</Text>
+                <Text style={{ color: "white", fontSize: 10, justifyContent: "center", alignSelf: "center" }} >{val.Gemeente + " " + val.Postcode}</Text>
+              </View>
+            </TouchableOpacity>
+          )
+        })}
+      </ScrollView>
+    </View>
+  )
+}
+
+//Functie Bottom navigatie
+const Tab = createBottomTabNavigator();
 export const BottomNavi = () => {
   return (
     <NavigationContainer>
@@ -72,162 +220,10 @@ export const BottomNavi = () => {
 }
 
 
-const Stack = createStackNavigator();
-
-export const MapStack = () => {
-  return (
-
-    <Stack.Navigator>
-      <Stack.Screen name="MapScreen" component={MapScreen} options={{
-        title: "Park & Ride Antwerpen", headerStyle: { backgroundColor: "red" }, headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }} />
-
-      <Stack.Screen name="DetailsMap" component={DetailMapButton} />
-    </Stack.Navigator>
-  )
-}
-
-
-export const ListStack = () => {
-  return (
-
-    <Stack.Navigator>
-      <Stack.Screen name="ListScreen" component={ListScreen} options={{
-        title: "Park & Ride Antwerpen", headerStyle: { backgroundColor: "red" }, headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }} />
-      <Stack.Screen name="DetailsMap" component={DetailMapButton} />
-    </Stack.Navigator>
-  )
-}
-
-
-
-export const DetailMapButton = ({ route }) => {
-
-  return (
-
-    <View style={styles.detailPaginaVanMap}>
-
-      <View style={styles.detailPaginaVanMapView}><Text style={{ color: "white", fontSize: 20 }}>{route.params.data.Naam}</Text></View>
-      <TextInput style={styles.detailPaginaTitle} editable={false} value={'Naam'} />
-      <Text style={styles.detailPaginaText}>{route.params.data.Naam}</Text>
-      <TextInput style={styles.detailPaginaTitle2} editable={false} value={'Adres'} />
-      <Text style={styles.detailPaginaText} >{route.params.data.Gemeente + " " + route.params.data.Postcode}</Text>
-      <TextInput style={styles.detailPaginaTitle2} editable={false} value={'Aantal Plaatsen'} />
-      <Text style={styles.detailPaginaText} >{route.params.data.Plaatsen}</Text>
-      <View style={styles.detailPaginaVanMapView}><Text style={{ color: "white", fontSize: 20 }}>Bereikbaar Met</Text></View>
-      <TextInput style={styles.detailPaginaTitle2} editable={false} value={'Buslijnen'} />
-      <Text style={styles.detailPaginaText} >{route.params.data.Buslijnen}</Text>
-      <TextInput style={styles.detailPaginaTitle2} editable={false} value={'Tramlijnen'} />
-      <Text style={styles.detailPaginaText} >{route.params.data.Tramlijnen}</Text>
-    </View>
-  )
-}
-
-
-export const MapScreen = ({ navigation }) => {
-
-  const [markerClickt, setMarkerClickt] = useState(false)
-  const [momdata, setMomdata] = useState("")
-  const [data, setData] = useState([]);
-  const [locatie, setLocatie] = useState("loading")
-
-  //
-  dataInladen(setData);
-
-  //
-  useEffect(() => {
-    (async () => {
-      let position = await Location.getCurrentPositionAsync();
-      setLocatie(position.coords);
-    })();
-  }, []);
-
-  const detailView = () => {
-    return (
-      setMarkerClickt(true)
-    )
-  }
-
-  const detailMarker = (val) => {
-    return (
-      <View style={styles.detailMarkerView}>
-        <Text style={{ color: "black", fontSize: 20, marginTop: 10 }}>{val.Naam}</Text>
-        <Text style={{ color: "black", fontSize: 18 }} >{val.Gemeente + " " + val.Postcode}</Text>
-        <Button style={{ backgroundColor: "#ff1a1a", width: "80%", marginTop: 20 }}
-          onPress={() => { navigation.navigate('DetailsMap', { data: val }) }}
-        ><Text style={{ color: "white" }}>Details</Text></Button>
-      </View>
-    )
-  }
-if (locatie == "loading") {
-  return <ActivityIndicator style={styles.dataLoading} animating={true} size="large" color={Colors.red800} />
-}
-else {
-  if (data == false) {
-    return <ActivityIndicator style={styles.dataLoading} animating={true} size="large" color={Colors.red800} />
-  } else {
-    return (
-      <View>
-        <MapView style={{ width: "100%", height: "100%", zIndex: 0 }} region={{
-          latitude: locatie.latitude,
-          longitude: locatie.longitude,
-          latitudeDelta: 0.5,
-          longitudeDelta: 0.5
-        }}>
-          {data.map(val => {
-            return (
-              <Marker
-                key={val.Naam}
-                coordinate={{ latitude: val.Latitude, longitude: val.Longitude }}
-                image={require('../Project/assets/parking.png')}
-                onPress={() => {
-                  detailView()
-                  setMomdata(val)
-                }}>
-              </Marker>
-            )
-          })}
-        </MapView>
-        {(markerClickt ? detailMarker(momdata) : <Text>False</Text>)}
-      </View>)
-  }
-}
-}
-
-
-export const ListScreen = ({ navigation }) => {
-
-  const [data, setData] = useState([]);
-  dataInladen(setData)
-
-
-  return (
-    <View style={{ marginTop: 15, backgroundColor: "white" }}>
-      <ScrollView>
-        {data.map((val) => {
-          return (
-            <TouchableOpacity key={val.OBJECTID} onPress={() => { navigation.navigate('DetailsMap', { data: val }) }}>
-              <View style={{ width: "100%", height: 50, margin: 5, backgroundColor: "red", }}>
-                <Text style={{ color: "white", fontSize: 15, marginTop: 10, justifyContent: "center", alignSelf: "center" }}>{val.Naam}</Text>
-                <Text style={{ color: "white", fontSize: 10, justifyContent: "center", alignSelf: "center" }} >{val.Gemeente + " " + val.Postcode}</Text>
-              </View>
-            </TouchableOpacity>
-          )
-        })}
-      </ScrollView>
-    </View>
-  )
-}
-
-
+//Functie App
 export default function App() {
+
+  //Permission voor locatie
   const [hasPermission, setHasPermission] = useState(null);
   useEffect(() => {
     (async () => {
@@ -242,15 +238,15 @@ export default function App() {
   if (hasPermission === false) {
     return <View style={{ flex: 1, justifyContent: 'center', alignItems: "center", alignSelf: "center" }}><Text>No access to location...</Text></View>
   }
+
+  //
   return (
     <BottomNavi />
 
   );
 }
 
-
-const Tab = createBottomTabNavigator();
-
+//Styles
 const styles = StyleSheet.create({
   container: {
   },
